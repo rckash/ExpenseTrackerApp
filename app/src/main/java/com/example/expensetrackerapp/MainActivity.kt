ba1c0a7fe05e9a6f23c3ab5a01211a070a1172ca
RC <rckashyapcpe@gmail.com>
2023-09-05 23:37:11 +0800

    added an uploadToFirestore function on Main Activity

--- app/src/main/java/com/example/expensetrackerapp/MainActivity.kt
+++ app/src/main/java/com/example/expensetrackerapp/MainActivity.kt
@@ -3,21 +3,32 @@ package com.example.expensetrackerapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.content.ContextCompat.startActivity
 import androidx.drawerlayout.widget.DrawerLayout
 import com.example.expensetrackerapp.databinding.ActivityMainBinding
 import com.example.expensetrackerapp.fragments.GoalsFragment
 import com.example.expensetrackerapp.fragments.HomeFragment
 import com.example.expensetrackerapp.fragments.ReportFragment
+import com.example.expensetrackerapp.roomdatabase.AppDatabase
 import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var auth: FirebaseAuth
+
+    private lateinit var appDB: AppDatabase
 
     lateinit var drawerToggle: ActionBarDrawerToggle
 
@@ -31,6 +42,9 @@ class MainActivity : AppCompatActivity() {
 
         // Initialize Firebase Auth
         auth = Firebase.auth
+
+        // database instantiation
+        appDB = AppDatabase.invoke(applicationContext)
 
         // Fragments Instantiation
         val HomeFragment = HomeFragment()
@@ -83,6 +97,50 @@ class MainActivity : AppCompatActivity() {
                     drawerLayout.close()
 
                 }
+                R.id.sync -> {
+
+                    val user = FirebaseAuth.getInstance().currentUser
+                    val userUid = user?.uid.toString()
+                    val db = Firebase.firestore
+
+                    GlobalScope.launch(Dispatchers.IO) {
+                        val expenses = db.collection("$userUid")
+
+                        for (expense in appDB.getExpenses().getAllExpenses()) {
+                            val city = hashMapOf(
+                                "name" to expense.name,
+                                "price" to expense.price,
+                                "category" to expense.category,
+                                "dateInt" to expense.dateInt,
+                                "dateString" to expense.dateString,
+                                "isExpense" to expense.isExpense
+                            )
+
+                            // Add a new document with a generated ID
+                            expenses.add(city)
+                                .addOnSuccessListener { documentReference ->
+                                    Toast.makeText(
+                                        applicationContext,
+                                        "Uploaded to Firebase",
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+                                    Log.d("TAG", "DocumentSnapshot added with ID: ${documentReference.id}")
+                                }
+                                .addOnFailureListener { e ->
+                                    Toast.makeText(
+                                        applicationContext,
+                                        "Failed to upload to Online Database",
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+                                    Log.w("TAG", "Error adding document", e)
+                                }
+                        }
+                        withContext(Dispatchers.Main) {
+                            Toast.makeText(applicationContext, "Synced", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+
+                }
                 R.id.sign_out -> {
                     auth.signOut()
 
@@ -93,6 +151,10 @@ class MainActivity : AppCompatActivity() {
             }
             true
         }
+
+    }
+
+    private fun uploadToFireStore() {
 
     }
 
